bc95855afb9d25ef4ee546303c1e3b326990cd35
Monica Bui <bui1@ualberta.ca>
2018-07-23 14:05:00 -0600

    checking if config changed

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 const os = require("os");
 const notes = require("./notes.js");
+const _ = require("lodash");
+
+console.log(_.isString("Andrew"));
+
+var filteredArray = _.uniq(["andrew","andrew",1,2,3,3,4]);
+console.log(filteredArray);
 
 var user = os.userInfo();
 
