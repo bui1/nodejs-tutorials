9ce0b53c13565e43c780bbe6565c4456f7133c71
Monica Bui <bui1@ualberta.ca>
2018-07-23 14:33:38 -0600

    playing around with yargs

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,17 +1,45 @@
+console.log("starting app.js");
 const fs = require('fs');
 const os = require("os");
 const notes = require("./notes.js");
 const _ = require("lodash");
+const yargs = require("yargs");
 
-console.log(_.isString("Andrew"));
+// console.log(_.isString("Andrew"));
 
-var filteredArray = _.uniq(["andrew","andrew",1,2,3,3,4]);
-console.log(filteredArray);
+// var filteredArray = _.uniq(["mike","andrew",1,2,3,3,4]);
+// console.log(filteredArray);
 
-var user = os.userInfo();
+// var user = os.userInfo();
 
-var res = notes.addNote();
+// var res = notes.addNote();
 
-var sum = notes.add(10,10);
+// var sum = notes.add(10,10);
 
-fs.appendFileSync('greetings.txt', 'Hello ' + user.username + '!');
+// fs.appendFileSync('greetings.txt', 'Hello ' + user.username + '!');
+var argv = yargs.argv;
+var command = process.argv[2];
+console.log(`Command: ${command}`);
+
+console.log(yargs.argv);
+
+if (command === "add"){
+    console.log("Adding new note");
+    notes.addNote(argv.title, argv.body);
+    // TO RUN THIS: node app.js add --title=secret --body="This is my secret"
+}
+else if (command === "read"){
+    console.log("reading a note");
+    notes.getNote(argv.title);
+}
+else if (command === "remove"){
+    console.log("Removing a note");
+    notes.removeNote(argv.title);
+}
+else if (command === "list"){
+    console.log("listing all notes");
+    notes.getAll();
+}
+else{
+    console.log("Not recognized");
+}
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,12 +1,35 @@
 console.log("starting notes.js");
 
-module.exports.age = 25;
+// module.exports.age = 25;
 
-module.exports.addNote = () => {
-    console.log("add new notes");
-    return "new notes";
+// module.exports.addNote = () => {
+//     console.log("add new notes");
+//     return "new notes";
+// };
+
+// module.exports.add = (a,b) => {
+//     return a+b;
+// };
+
+var addNote = (title, body) => {
+    console.log("Adding note", title, body);
 };
 
-module.exports.add = (a,b) => {
-    return a+b;
+var getAll= () =>{
+    console.log("Getting all notes");
+};
+
+var getNote = (title) => {
+    console.log("Getting title", title);
+}
+
+var removeNote = (title) => {
+    console.log("Removing note: ", title);
+}
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
 };
